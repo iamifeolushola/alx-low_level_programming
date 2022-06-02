549c5f68e654c363c0b866b9029e7c2f7bbc4d62
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-02 09:32:01 -0700

    Fixed code style error & output error in task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 when properly exited, otherwise non-zero.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
