3cf37b79d8d653320f0ce2c9c867f9a54b37916b
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-08 14:42:21 +0100

    Added tasks 0 - 2 to repo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
