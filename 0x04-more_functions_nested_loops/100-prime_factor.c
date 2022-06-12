81844f1a95a7192c2790748967f86a58824cc25d
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-12 03:04:09 +0100

    Update repo

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,22 +1,39 @@
-#include <stdio.h>
-
-/**
- * main - prints largest prime factor
- * Return: 0 Always (Success)
- */
-int main(void)
-{
-	long int n, fp;
-	
-	n = 612852475143;
-	for (fp = 2; fp <= n; fp++)
-	{
-		if (n % fp == 0)
-		{
-			n /= fp;
-			fp--;
-		}
-	}
-	printf("%ld\n", fp);
-	return (0);
-}
+#include <stdio.h> 
+#include <math.h> 
+  
+ /** 
+  * main - prints the largest prime factor
+  * followed by a new line 
+  * Return: Always 0 (Success) 
+  */ 
+ int main(void) 
+ { 
+         long int n; 
+         long int max; 
+         long int i; 
+  
+         n = 612852475143; 
+         max = -1; 
+  
+         while (n % 2 == 0) 
+         { 
+                 max = 2; 
+                 n /= 2; 
+         } 
+  
+         for (i = 3; i <= sqrt(n); i = i + 2) 
+         { 
+                 while (n % i == 0) 
+                 { 
+                         max = i; 
+                         n = n / i; 
+                 } 
+         } 
+  
+         if (n > 2) 
+                 max = n; 
+  
+         printf("%ld\n", max); 
+  
+         return (0); 
+ }
