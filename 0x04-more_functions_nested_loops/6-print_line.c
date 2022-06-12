40a1f1b9cf0b3ce755f4ac04fc4281e68d396bda
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-12 17:34:47 +0100

    Fix for task 6 & 100

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - this prints straight line `n` times
+ * @n: the number of times the line will print
+ * Return: none.
+ */
+void print_line(int n) 
+ { 
+         int i; 
+  
+         for (i = 0; i < n; i++) 
+         { 
+                 _putchar(95); 
+         } 
+         _putchar('\n'); 
+ }
