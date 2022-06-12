5325fedf4d43dc25e994f952ce1497e5a9e1d6cd
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-12 02:42:36 +0100

    Fixed error in task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,19 @@
  * Return: no return.
  */
 void print_line(int n)
-{
-	int i;
+ {
+         int i;
 
-	for (i = 0; i < n; i++)
-	{
-		_putchar(95);
-	}
-	_putchar('\n');
-}
+         if (n <= 0)
+         {
+                 _putchar('\n');
+         }
+         else
+         {
+                 for (i = 1; i <= n; i++)
+                 {
+                         _putchar('_');
+                 }
+                 _putchar('\n');
+         }
+ }
