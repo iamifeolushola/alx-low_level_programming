b851e844f87c74e019a4e195c94e9591b66550b5
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-13 20:21:47 +0100

    Added files for the project, main.h & README to repo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates its value
+ * @n: the integer
+ * Return: none.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# 0x05. Pointers, Arrays, Strings
