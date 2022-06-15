88ac8c09d1c4643b85c395f4a3f130f16fb5469c
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-15 20:02:29 +0100

    Added file 7-leet to repo

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.c"
+/**
+ * leet - this function encodes a string into 1337
+ * @s: a string
+ * Return: the pointer to dest
+ */
+char *leet(char *s)
+{
+	int count = 0, i;
+	int lw_letters[] = {97, 101, 111, 116, 108};
+	int up_letters[] = {65, 69, 79, 84, 76};
+	int num[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == lw_letters[i] || *(s + coun) == up_letters[i])
+			{
+				*(s + count) = num[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
