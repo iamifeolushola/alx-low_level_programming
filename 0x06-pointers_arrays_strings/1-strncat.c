f39c796a79ab2d74140d32656052cd9b2e4b2ae3
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-15 18:32:03 +0100

    Task files 0-strcat & 1-strncat added to repo

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat: this function concatenates two strings
+ * @src: the source
+ * @dest: the destination
+ * @n: total amount of bytes used from `src`
+ * Return: the pointer to @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
