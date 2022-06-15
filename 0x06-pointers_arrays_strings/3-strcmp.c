9a07c30d823e322a68d3dc2fa3d2cd35a94b338c
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-15 20:09:32 +0100

    Fixed error in file 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, cp = 0;
+	int i = 0, op = 0;
 
-	while (cp == 0)
+	while (op == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		cp = *(s1 + i) - *(s2 + 1);
+		op = *(s1 + i) - *(s2 + 1);
 		i++;
 	}
 
-	return (cp);
+	return (op);
 }
