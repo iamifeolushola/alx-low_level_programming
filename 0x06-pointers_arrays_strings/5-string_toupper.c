6478f3d5359f5805d068cc3c2d836dfbd178cccc
iamifeolushola <iam.ifeolushola@gmail.com>
2022-06-15 19:36:03 +0100

    Added file 5-string_toupper to repo

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - this function changes all lowercase letters
+ * of a string to uppercase
+ * @s: a string
+ * Return: a string
+ */
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 112))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+
+	return (s);
+}
